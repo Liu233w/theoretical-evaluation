4f201d52e5e35d0f7a7fd72e6ca69baef19d4b08
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-12-13 22:32:43 +0800

    多线程运行 tcas

--- src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/RunTcas.java
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/RunTcas.java
@@ -14,6 +14,7 @@ import edu.nwpu.machunyan.theoreticalEvaluation.runningDatas.Program;
 import edu.nwpu.machunyan.theoreticalEvaluation.runningDatas.SingleRunResult;
 import edu.nwpu.machunyan.theoreticalEvaluation.utils.FileUtils;
 import edu.nwpu.machunyan.theoreticalEvaluation.utils.LogUtils;
+import sun.rmi.runtime.Log;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -22,19 +23,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.concurrent.*;
 
 public class RunTcas {
 
-    public static void main(String[] args) throws URISyntaxException, IOException, CoverageRunnerException {
+    // 准备好的测试用例
+    private static ArrayList<IProgramInput> testCases;
+    // 程序文件夹
+    private static Path versionsDir;
+    // 一共多少个版本
+    private static final int lastVersionNum = 41;
 
-        final ArrayList<IProgramInput> testCases = buildTestCases();
+    static {
+        try {
+            testCases = buildTestCasesObject();
+            versionsDir = FileUtils.getFilePathFromResources("tcas/versions");
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        // 程序文件夹
-        final Path versionsDir = FileUtils.getFilePathFromResources("tcas/versions");
-        // 一共多少个版本
-        final int lastVersionNum = 41;
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        // 输出结果
+        final int availableProcessors = Runtime.getRuntime().availableProcessors();
+        LogUtils.logInfo("ready to start, thread pool size: " + availableProcessors);
+        final ExecutorService threadPool = Executors.newFixedThreadPool(availableProcessors);
+
         final JsonArray output = new JsonArray();
 
         for (int versionNum = 1; versionNum <= lastVersionNum; versionNum++) {
@@ -42,46 +56,69 @@ public class RunTcas {
             // 准备当前版本的程序
             final String versionNumString = "v" + versionNum;
             final Path sourceFilePath = versionsDir.resolve(versionNumString).resolve("tcas.c");
-            final RunningScheduler runningScheduler = new RunningScheduler(
-                    new Program(versionNumString, sourceFilePath.toString()),
-                    new GccReadFromStdIoRunner(),
-                    testCases);
 
-            // 使用测试用例运行程序
-            LogUtils.logInfo("running version: " + versionNumString);
-            final ArrayList<SingleRunResult> runResults = runningScheduler.runAndGetResults();
-            final long passedCount = runResults.stream().filter(SingleRunResult::isCorrect).count();
-
-            LogUtils.logInfo("passed test case count: " + passedCount);
-
-            final ArrayList<VectorTableModelRecord> vectorTableModel = VectorTableModelGenerator.generateVectorTableModelFromRunResult(runResults);
-
-            // 某行代码的错误率（排序过的）
-            final ArrayList<SuspiciousnessFactorRecord> factorOfO = SuspiciousnessFactorGenerator.getSuspiciousnessFactorMatrixOrdered(
-                    vectorTableModel,
-                    record -> (double) record.calculateSuspiciousnessFactorAsO()
-            );
-            final ArrayList<SuspiciousnessFactorRecord> factorOfOp = SuspiciousnessFactorGenerator.getSuspiciousnessFactorMatrixOrdered(
-                    vectorTableModel,
-                    record -> record.calculateSuspiciousnessFactorAsOp(testCases.size(), (int) passedCount)
-            );
-
-            // 输出结果
+            // 多线程运行程序
             final JsonObject resultRecord = new JsonObject();
-            resultRecord.add("version", new JsonPrimitive(versionNumString));
-
-            resultRecord.add("factors of O", new Gson().toJsonTree(factorOfO));
-            resultRecord.add("factors of Op", new Gson().toJsonTree(factorOfOp));
-
             output.add(resultRecord);
+
+            threadPool.submit(() -> {
+                try {
+                    runAndFillJsonObject(versionNumString, sourceFilePath, resultRecord);
+                } catch (CoverageRunnerException e) {
+                    e.printStackTrace();
+                }
+            });
         }
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 
         final Gson gson = new GsonBuilder().setPrettyPrinting().create();
         final byte[] outputBytes = gson.toJson(output).getBytes();
         Files.write(Paths.get("suspiciousness-factors.json"), outputBytes);
     }
 
-    private static ArrayList<IProgramInput> buildTestCases() throws URISyntaxException, IOException {
+    /**
+     * 运行某个版本程序的所有测试用例，用结果填充 json string
+     *
+     * @param versionNumString
+     * @param sourceFilePath
+     * @param resultRecord
+     * @throws CoverageRunnerException
+     */
+    private static void runAndFillJsonObject(String versionNumString, Path sourceFilePath, JsonObject resultRecord) throws CoverageRunnerException {
+
+        final RunningScheduler runningScheduler = new RunningScheduler(
+                new Program(versionNumString, sourceFilePath.toString()),
+                new GccReadFromStdIoRunner(),
+                testCases);
+
+        // 使用测试用例运行程序
+        LogUtils.logInfo("start running version: " + versionNumString);
+        final ArrayList<SingleRunResult> runResults = runningScheduler.runAndGetResults();
+        final long passedCount = runResults.stream().filter(SingleRunResult::isCorrect).count();
+
+        LogUtils.logInfo("passed test case count for version " + versionNumString + ": " + passedCount);
+
+        final ArrayList<VectorTableModelRecord> vectorTableModel = VectorTableModelGenerator.generateVectorTableModelFromRunResult(runResults);
+
+        // 某行代码的错误率（排序过的）
+        final ArrayList<SuspiciousnessFactorRecord> factorOfO = SuspiciousnessFactorGenerator.getSuspiciousnessFactorMatrixOrdered(
+                vectorTableModel,
+                record -> (double) record.calculateSuspiciousnessFactorAsO()
+        );
+        final ArrayList<SuspiciousnessFactorRecord> factorOfOp = SuspiciousnessFactorGenerator.getSuspiciousnessFactorMatrixOrdered(
+                vectorTableModel,
+                record -> record.calculateSuspiciousnessFactorAsOp(testCases.size(), (int) passedCount)
+        );
+
+        //输出结果
+        resultRecord.add("version", new JsonPrimitive(versionNumString));
+        resultRecord.add("factors of O", new Gson().toJsonTree(factorOfO));
+        resultRecord.add("factors of Op", new Gson().toJsonTree(factorOfOp));
+    }
+
+    private static ArrayList<IProgramInput> buildTestCasesObject() throws URISyntaxException, IOException {
 
         final Path casePath = FileUtils.getFilePathFromResources("tcas/testplans/cases.json");
         final InputStreamReader jsonReader = new InputStreamReader(Files.newInputStream(casePath));
