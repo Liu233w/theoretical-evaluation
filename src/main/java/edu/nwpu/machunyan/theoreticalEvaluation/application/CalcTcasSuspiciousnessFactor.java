e030b298522064530254b44e2e28cc945b349be0
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-12-15 16:34:39 +0800

    重新添加 suspiciousness factor 的生成代码，从中间结果读取进行计算

---
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/CalcTcasSuspiciousnessFactor.java
@@ -0,0 +1,64 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.application;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorGenerator;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorRecord;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.VectorTableModelGenerator;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.VectorTableModelRecord;
+import edu.nwpu.machunyan.theoreticalEvaluation.runningDatas.SingleRunResult;
+import edu.nwpu.machunyan.theoreticalEvaluation.utils.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * 计算 tcas 的 suspiciousness factor，请保证上一步输出的结果文件存在
+ */
+public class CalcTcasSuspiciousnessFactor {
+
+    // 结果的输出路径
+    private static final String resultOutputPath = "./target/outputs/suspiciousness-factors.json";
+
+    public static void main(String[] args) throws IOException {
+
+        final HashMap<String, ArrayList<SingleRunResult>> runResultsForVersion = RunTcas.getRunResultsFromSavedFile();
+
+        final int versionCount = runResultsForVersion.size();
+
+        final JsonArray result = new JsonArray(versionCount);
+        for (int i = 1; i <= versionCount; i++) {
+
+            final String versionNumStr = "v" + i;
+            final ArrayList<SingleRunResult> runResults = runResultsForVersion.get(versionNumStr);
+
+            final long passedCount = runResults.stream().filter(SingleRunResult::isCorrect).count();
+            final ArrayList<VectorTableModelRecord> vectorTableModel = VectorTableModelGenerator.generateVectorTableModelFromRunResult(runResults);
+
+            // 某行代码的错误率（排序过的）
+            final ArrayList<SuspiciousnessFactorRecord> factorOfO = SuspiciousnessFactorGenerator.getSuspiciousnessFactorMatrixOrdered(
+                    vectorTableModel,
+                    record -> (double) record.calculateSuspiciousnessFactorAsO()
+            );
+            final ArrayList<SuspiciousnessFactorRecord> factorOfOp = SuspiciousnessFactorGenerator.getSuspiciousnessFactorMatrixOrdered(
+                    vectorTableModel,
+                    record -> record.calculateSuspiciousnessFactorAsOp(runResults.size(), (int) passedCount)
+            );
+
+            // 记录结果
+            final JsonObject resultRecord = new JsonObject();
+            resultRecord.add("version", new JsonPrimitive(versionNumStr));
+            resultRecord.add("factors of O", new Gson().toJsonTree(factorOfO));
+            resultRecord.add("factors of Op", new Gson().toJsonTree(factorOfOp));
+
+            result.add(resultRecord);
+        }
+
+        FileUtils.printJsonToFile(Paths.get(resultOutputPath), result);
+    }
+
+}
