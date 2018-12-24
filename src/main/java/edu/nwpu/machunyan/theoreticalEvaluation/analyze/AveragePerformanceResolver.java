9152f9b38533deb5d9b50d7ded6a467018f4cc6d
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-12-24 17:08:03 +0800

    add AveragePerformanceResolver

---
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/analyze/AveragePerformanceResolver.java
@@ -0,0 +1,53 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.analyze;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * 从 {@link OrderedVectorTableModel} 中生成测试用例的平均性能
+ */
+public class AveragePerformanceResolver {
+
+    /**
+     * 从 vtm 中生成 II-set1 分区信息，包含每个分区的长度（只有长度大于1才被称为一个分区）
+     *
+     * @param vtm
+     * @return
+     */
+    public static ArrayList<Integer> resolvePartition(OrderedVectorTableModel vtm) {
+        final ArrayList<Integer> result = new ArrayList<>();
+
+        int lastMatchedAnp = -1;
+        int partitionDegree = 0;
+        for (int i = vtm.getIIset1BeginPosition(); i < vtm.getIIset2BeginPosition(); i++) {
+            final int thisAnp = vtm.getVectorTableModel().get(i).getAnp();
+            if (thisAnp == lastMatchedAnp) {
+                ++partitionDegree;
+            } else {
+                lastMatchedAnp = thisAnp;
+                if (partitionDegree > 1) {
+                    result.add(partitionDegree);
+                }
+                partitionDegree = 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static double resolveAveragePerformance(OrderedVectorTableModel vtm) {
+
+        final ArrayList<Integer> partitions = resolvePartition(vtm);
+        final double x = vtm.getIIset1BeginPosition();
+        final double z = vtm.getIIset2BeginPosition() - x;
+        final double n = vtm.getVectorTableModel().size();
+
+        int sumPartitions = partitions.stream().reduce(0, Integer::sum);
+
+        final double result = (Math.pow((x + z), 2) - z + sumPartitions)
+                /
+                (2 * n * (x + z));
+
+        return result;
+    }
+}
---
+++ src/test/java/edu/nwpu/machunyan/theoreticalEvaluation/analyze/AveragePerformanceResolverTest.kt
@@ -0,0 +1,39 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.analyze
+
+import io.kotlintest.matchers.plusOrMinus
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FreeSpec
+
+class AveragePerformanceResolverTest : FreeSpec({
+
+    val vectorTableModel = buildVectorModelTableFromMatrix(
+            arrayOf(0, 0, 3, 6),
+            arrayOf(1, 0, 2, 6),
+            arrayOf(0, 4, 3, 2),
+            arrayOf(0, 0, 3, 6),
+            arrayOf(0, 3, 3, 3),
+            arrayOf(0, 3, 3, 3),
+            arrayOf(0, 0, 3, 6),
+            arrayOf(2, 0, 1, 6),
+            arrayOf(0, 0, 3, 6),
+            arrayOf(0, 3, 3, 3)
+    )
+    val orderedVectorTableModel = OrderedVectorTableModel.fromVectorTableModel(vectorTableModel)
+
+    "resolvePartition" - {
+        "能通过论文中的测试数据" {
+            val partitions = AveragePerformanceResolver.resolvePartition(orderedVectorTableModel)
+            partitions shouldBe arrayListOf(3)
+        }
+    }
+
+    "resolveAveragePerformance" - {
+        "能通过论文中的测试数据" - {
+            //  II −set1 = s5, s6, s10, s3, x = 4, z = 4, k1 = 3, k2 = 3, k3 = 3, and k4 = 4 for PG1 in Fig. 1,
+            //  so y = 1, ∆1 = 3, and the average performance of O for PG1 is ((4+4)2−4+3)/2×10×8 = 63/160.
+
+            val result = AveragePerformanceResolver.resolveAveragePerformance(orderedVectorTableModel)
+            result shouldBe (63.0/160 plusOrMinus 0.0000001)
+        }
+    }
+})
