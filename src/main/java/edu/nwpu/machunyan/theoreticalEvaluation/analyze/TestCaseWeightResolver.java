530ddb861608eac4de97770246877542d0754f6a
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-12-24 18:33:52 +0800

    add TestCaseWeightResolver

---
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/analyze/TestCaseWeightResolver.java
@@ -0,0 +1,76 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.analyze;
+
+import edu.nwpu.machunyan.theoreticalEvaluation.runningDatas.SingleRunResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * 计算出测试用例的权重
+ */
+public class TestCaseWeightResolver {
+
+    /**
+     * 从一系列运行结果（测试用例）中计算出测试用例的权重
+     *
+     * @param runResults
+     * @return
+     */
+    public static ArrayList<Double> resolveTestCaseWeight(List<SingleRunResult> runResults) {
+
+        final int statementCount = runResults.get(0).getStatementMap().getStatementCount();
+        final double overall = resolveAveragePerformance(runResults.stream(), statementCount);
+
+        final ArrayList<Double> result = new ArrayList<>(runResults.size());
+
+        for (int i = 0; i < runResults.size(); i++) {
+            final Stream<SingleRunResult> skippedStream = ofStreamSkipAtIndex(runResults, i);
+            final double averagePerformance = resolveAveragePerformance(skippedStream, statementCount);
+            result.add(averagePerformance);
+        }
+
+        final Double improvedAverage = result.stream().filter(a -> a - overall < 0).reduce(0.0, Double::sum);
+        final Double reducedAverage = result.stream().filter(a -> a - overall > 0).reduce(0.0, Double::sum);
+
+        for (int i = 0; i < result.size(); i++) {
+            final Double a = result.get(i) - overall;
+            if (a < 0) {
+                result.set(i, a / improvedAverage);
+            } else if (a > 0) {
+                result.set(i, a / reducedAverage + 1);
+            } else {
+                result.set(i, 0.0);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 从测试用例中生成一个流，跳过特定某个位置的测试用例。
+     *
+     * @param runResults
+     * @param index
+     * @return
+     */
+    private static Stream<SingleRunResult> ofStreamSkipAtIndex(List<SingleRunResult> runResults, int index) {
+        return IntStream.range(0, runResults.size())
+                .filter(i -> i != index)
+                .mapToObj(runResults::get);
+    }
+
+    /**
+     * 从测试用例运行结果计算平均性能
+     *
+     * @param testCases
+     * @param statementCount 语句数量
+     * @return
+     */
+    private static double resolveAveragePerformance(Stream<SingleRunResult> testCases, int statementCount) {
+        final ArrayList<VectorTableModelRecord> vtm = VectorTableModelGenerator.generateFromStream(testCases, statementCount);
+        final OrderedVectorTableModel orderedVtm = OrderedVectorTableModel.fromVectorTableModel(vtm);
+        return AveragePerformanceResolver.resolveAveragePerformance(orderedVtm);
+    }
+}
--- src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/analyze/VectorTableModelGenerator.java
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/analyze/VectorTableModelGenerator.java
@@ -1,9 +1,11 @@
 package edu.nwpu.machunyan.theoreticalEvaluation.analyze;
 
 import edu.nwpu.machunyan.theoreticalEvaluation.runningDatas.SingleRunResult;
+import edu.nwpu.machunyan.theoreticalEvaluation.runningDatas.StatementMap;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class VectorTableModelGenerator {
 
@@ -14,8 +16,17 @@ public class VectorTableModelGenerator {
      * @return vector table model，对应一个矩阵。第 0 个元素为 null，从第一个元素开始每个元素表示一行。
      */
     public static ArrayList<VectorTableModelRecord> generateFromRunResult(List<SingleRunResult> runResults) {
+        return generateFromStream(runResults.stream(), runResults.get(0).getStatementMap().getStatementCount());
+    }
 
-        final int statementCount = runResults.get(0).getStatementMap().getStatementCount();
+    /**
+     * 从一个表示运行结果的流中得出 vector table model。每个运行结果的 statementMap 必须一致。
+     *
+     * @param stream
+     * @param statementCount 总体的语句数量
+     * @return vector table model，对应一个矩阵。第 0 个元素为 null，从第一个元素开始每个元素表示一行。
+     */
+    public static ArrayList<VectorTableModelRecord> generateFromStream(Stream<SingleRunResult> stream, int statementCount) {
 
         final ArrayList<VectorTableModelRecordBuilder> builders = new ArrayList<>(statementCount);
 
@@ -23,12 +34,11 @@ public class VectorTableModelGenerator {
             builders.add(new VectorTableModelRecordBuilder(i + 1));
         }
 
-        for (SingleRunResult singleRunResult :
-                runResults) {
+        stream.forEach(singleRunResult -> {
             for (int i = 0; i < statementCount; i++) {
                 builders.get(i).processSingleRunResult(singleRunResult);
             }
-        }
+        });
 
         final ArrayList<VectorTableModelRecord> result = new ArrayList<>(statementCount + 1);
         result.add(null);
@@ -39,5 +49,4 @@ public class VectorTableModelGenerator {
 
         return result;
     }
-
 }
