4c53cb09869500f50f4db883be1c7e16fe937ec4
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-01-06 16:25:18 +0800

    完成所有的公式

--- src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/analyze/SuspiciousnessFactorFormulas.java
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/analyze/SuspiciousnessFactorFormulas.java
@@ -172,4 +172,180 @@ public class SuspiciousnessFactorFormulas {
             /
             ((aef + f + aep) * (anp + anf + p));
     }
+
+    public static double ample(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return Math.abs(aef / f - aep / p);
+    }
+
+    public static double arithmeticMean(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double anp = record.getAnp();
+        final double anf = record.getAnf();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return (2 * aef * anp - 2 * anf * aep)
+            /
+            ((aef + aep) * (anf + anp) + f * p);
+    }
+
+    public static double cohen(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double anp = record.getAnp();
+        final double anf = record.getAnf();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return (2 * aef * anp - 2 * anf * aep)
+            /
+            ((aef + aep) * p + (anf + anp) * f);
+    }
+
+    public static double fleiss(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double anp = record.getAnp();
+        final double anf = record.getAnf();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return (4 * aef * anp - 2 * anf * aep - anf * anf - aep * aep)
+            /
+            (2 * f + 2 * p);
+    }
+
+    public static double m1(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double anp = record.getAnp();
+        final double f = resolveF(record);
+
+        return (aef + anp) / (f - aef + aep);
+    }
+
+    public static double wong3(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+
+        if (aep <= 2) {
+            return aef - aep;
+        } else if (aep <= 10) {
+            return aef - (2 + 0.1 * (aep - 2));
+        } else {
+            return aef - (2.8 + 0.001 * (aep - 10));
+        }
+    }
+
+    public static double naish2(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double p = resolveP(record);
+
+        return aef - aep / (p + 1);
+    }
+
+    public static double dice(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+
+        return 2 * aef / (f + aep);
+    }
+
+    public static double jaccard(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+
+        return aef / (f + aep);
+    }
+
+    public static double qe(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+
+        return aef / (aef + aep);
+    }
+
+    public static double euclid(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double anp = record.getAnp();
+        final double anf = record.getAnf();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return Math.sqrt(aef + anp);
+    }
+
+    public static double hammingEtc(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double anp = record.getAnp();
+
+        return aef + anp;
+    }
+
+    public static double wong2(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+
+        return aef - aep;
+    }
+
+    public static double russelRao(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return aef / (f + p);
+    }
+
+    public static double ample2(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return aef / f - aep / p;
+    }
+
+    public static double kulcznski1(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+
+        return aef / (f - aef + aep);
+    }
+
+    public static double kulcznski2(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+
+        return aef / (2 * f) + aef / (2 * aef + 2 * aep);
+    }
+
+    public static double ochiai(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+
+        return aef / Math.sqrt(f * (aef + aep));
+    }
+
+    public static double m2(VectorTableModelRecord record) {
+        final double aef = record.getAef();
+        final double aep = record.getAep();
+        final double f = resolveF(record);
+        final double p = resolveP(record);
+
+        return aef / (2 * f - aef + p + aep);
+    }
 }
