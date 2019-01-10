d1606d665172b1f8664d28bcd75973dca40c7ff2
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-01-10 23:10:23 +0800

    添加一个 benchmark 用来确定在哪里开启 TestcaseWeightResolver 的并发

---
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/temporary/TestcaseWeightResolverBenchmark.java
@@ -0,0 +1,30 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.application.temporary;
+
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorFormulas;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.TestcaseWeightResolver;
+import edu.nwpu.machunyan.theoreticalEvaluation.application.RunTotInfo;
+import edu.nwpu.machunyan.theoreticalEvaluation.runner.pojo.RunResultJam;
+
+import java.io.FileNotFoundException;
+
+public class TestcaseWeightResolverBenchmark {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        final RunResultJam imports = RunTotInfo.getRunResultsFromSavedFile();
+
+        final TestcaseWeightResolver resolver = new TestcaseWeightResolver(SuspiciousnessFactorFormulas::o);
+
+        // Code took 7.4316307 seconds
+        System.out.println("a program");
+        bench(() -> resolver.resolve(imports.getRunResultForPrograms().get(0)));
+    }
+
+    private static void bench(Runnable runnable) {
+
+        final long endTime, startTime = System.nanoTime();
+        runnable.run();
+        endTime = System.nanoTime();
+        System.out.println("Code took " + (endTime - startTime) / 1000000000.0 + " seconds");
+    }
+}
