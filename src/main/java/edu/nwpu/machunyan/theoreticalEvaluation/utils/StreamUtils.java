b89f3c2ec80324e0505883f9eedf475240aaf5ba
Liu233w <wwwlsmcom@outlook.com>
2019-01-21 15:21:18 +0800

    移除 StreamUtils 中的 Java8 Stream 相关方法
    用 StreamEx 代替即可

--- src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/utils/StreamUtils.java
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/utils/StreamUtils.java
@@ -1,12 +1,7 @@
 package edu.nwpu.machunyan.theoreticalEvaluation.utils;
 
 import java.io.InputStream;
-import java.util.AbstractMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class StreamUtils {
 
@@ -19,34 +14,5 @@ public class StreamUtils {
     public static String convertStreamToString(InputStream is) {
         Scanner s = new Scanner(is).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
-    }
-
-    public static <T> Stream<T> asStream(Iterator<T> sourceIterator) {
-        return asStream(sourceIterator, false);
-    }
-
-    public static <T> Stream<T> asStream(Iterator<T> sourceIterator, boolean parallel) {
-        Iterable<T> iterable = () -> sourceIterator;
-        return StreamSupport.stream(iterable.spliterator(), parallel);
-    }
-
-    /**
-     * Zips the specified stream with its indices.
-     */
-    public static <T> Stream<Map.Entry<Integer, T>> zipWithIndex(Stream<? extends T> stream) {
-        return asStream(new Iterator<Map.Entry<Integer, T>>() {
-            private final Iterator<? extends T> streamIterator = stream.iterator();
-            private int index = 0;
-
-            @Override
-            public boolean hasNext() {
-                return streamIterator.hasNext();
-            }
-
-            @Override
-            public Map.Entry<Integer, T> next() {
-                return new AbstractMap.SimpleImmutableEntry<>(index++, streamIterator.next());
-            }
-        });
     }
 }
