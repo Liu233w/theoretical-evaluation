5dfc9546a4f25eee54a229b765894d46b8da44a4
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-04-13 15:33:36 +0800

    完成另外两个批量运行程序

---
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/ResolveSuspiciousnessFactor.java
@@ -0,0 +1,72 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.application;
+
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorFormulas;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorResolver;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorUtils;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.VectorTableModelResolver;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.MultipleFormulaSuspiciousnessFactorJam;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.SuspiciousnessFactorJam;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.VectorTableModelJam;
+import edu.nwpu.machunyan.theoreticalEvaluation.runner.pojo.RunResultJam;
+import edu.nwpu.machunyan.theoreticalEvaluation.utils.CsvExporter;
+import edu.nwpu.machunyan.theoreticalEvaluation.utils.FileUtils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 获取
+ */
+public class ResolveSuspiciousnessFactor {
+
+    // 输出文件
+    private static final String jsonOutputDir = "./target/outputs/suspiciousness-factors-json";
+    private static final String csvOutputDir = "./target/outputs/suspiciousness-factors-csv";
+
+    private static final String[] MAIN_LIST = new String[]{
+        "my_sort",
+        "schedule2",
+        "tcas",
+        "tot_info",
+    };
+
+    public static void main(String[] args) throws IOException {
+
+        for (String name : MAIN_LIST) {
+            final SuspiciousnessFactorJam json = runAndGetResult(name);
+            final MultipleFormulaSuspiciousnessFactorJam csv = SuspiciousnessFactorUtils.collectAsMultipleFormula(json);
+
+            FileUtils.saveObject(resolveResultFilePath(name), json);
+            FileUtils.saveString(resolveCsvFilePath(name), CsvExporter.toCsvString(csv));
+        }
+    }
+
+    public static SuspiciousnessFactorJam getResultFromFile(String programName) throws FileNotFoundException {
+        final String path = resolveResultFilePath(programName);
+        return FileUtils.loadObject(path, SuspiciousnessFactorJam.class);
+    }
+
+    public static SuspiciousnessFactorJam runAndGetResult(String programName) throws FileNotFoundException {
+
+        final RunResultJam jam = Run.getResultFromFile(programName);
+
+        final List<SuspiciousnessFactorResolver> resolvers = SuspiciousnessFactorResolver.of(
+            SuspiciousnessFactorFormulas.getAllFormulas()
+        );
+
+        final VectorTableModelJam vtm = VectorTableModelResolver.resolve(jam);
+
+        return SuspiciousnessFactorUtils.runOnAllResolvers(vtm, resolvers);
+    }
+
+
+    private static String resolveResultFilePath(String programName) {
+        return jsonOutputDir + "/" + programName + ".json";
+    }
+
+    private static String resolveCsvFilePath(String programName) {
+        return csvOutputDir + "/" + programName + ".csv";
+    }
+}
+
---
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/ResolveTestcaseWeight.java
@@ -0,0 +1,49 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.application;
+
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorFormulas;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.TestcaseWeightHelper;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.TestcaseWeightResolver;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.TestcaseWeightJam;
+import edu.nwpu.machunyan.theoreticalEvaluation.runner.pojo.RunResultJam;
+import edu.nwpu.machunyan.theoreticalEvaluation.utils.FileUtils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class ResolveTestcaseWeight {
+
+    private static final String resultDir = "./target/outputs/testcase-weights";
+
+    private static final String[] MAIN_LIST = new String[]{
+        "my_sort",
+        "schedule2",
+        "tcas",
+        "tot_info",
+    };
+
+    public static void main(String[] args) throws IOException {
+
+        for (String name : MAIN_LIST) {
+
+            System.out.println("Running on " + name);
+
+            final RunResultJam imports = Run.getResultFromFile(name);
+            final List<TestcaseWeightResolver> resolver = TestcaseWeightResolver.of(SuspiciousnessFactorFormulas.getAllFormulas());
+            final TestcaseWeightJam result = TestcaseWeightHelper.runOnAllResolvers(imports, resolver);
+
+            FileUtils.saveObject(resolveResultFilePath(name), result);
+        }
+    }
+
+    public static TestcaseWeightJam getResultFromFile(String programName) throws FileNotFoundException {
+        final String path = resolveResultFilePath(programName);
+        return FileUtils.loadObject(path, TestcaseWeightJam.class);
+    }
+
+
+    private static String resolveResultFilePath(String programName) {
+        return resultDir + "/" + programName + ".json";
+    }
+
+}
