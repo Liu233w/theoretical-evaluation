1651336d179f0d72b4c142f3dfcec24375f93f07
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-04-16 00:26:10 +0800

    完成重构比较代码
    看起来重构之后的 AveragePerformanceResolver 的平均值计算和之前不大一样。
    DoubleStream::average 的计算结果和手动计算的不一样，但相差很小，应该没有影响。

--- src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/temporary/CompareTwoTestcaseWeight.java
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/temporary/CompareTwoTestcaseWeight.java
@@ -3,6 +3,7 @@ package edu.nwpu.machunyan.theoreticalEvaluation.application.temporary;
 import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.TestcaseWeightForProgram;
 import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.TestcaseWeightForTestcase;
 import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.TestcaseWeightJam;
+import edu.nwpu.machunyan.theoreticalEvaluation.application.ResolveTestcaseWeight;
 import edu.nwpu.machunyan.theoreticalEvaluation.utils.FileUtils;
 import lombok.val;
 import one.util.streamex.EntryStream;
@@ -15,12 +16,8 @@ import java.util.Map;
 public class CompareTwoTestcaseWeight {
     public static void main(String[] args) throws IOException {
 
-        final TestcaseWeightJam left = FileUtils.loadObject(
-            "./target/outputs/tot_info-testcase-weight.json",
-            TestcaseWeightJam.class);
-        final TestcaseWeightJam right = FileUtils.loadObject(
-            "./target/outputs/tot_info-testcase-weight - 副本.json",
-            TestcaseWeightJam.class);
+        final TestcaseWeightJam left = ResolveTestcaseWeight.getResultFromFile("tot_info - 1");
+        final TestcaseWeightJam right = ResolveTestcaseWeight.getResultFromFile("tot_info - 2");
 
         if (left.equals(right)) {
 
@@ -45,13 +42,15 @@ public class CompareTwoTestcaseWeight {
                 a -> a.getTitle() + "-" + a.getFormulaTitle(),
                 TestcaseWeightForProgram::getTestcaseWeights
             );
+
+        double epsilon = 0.0000001;
         return StreamEx
             .of(right.getTestcaseWeightForPrograms())
             .map(a -> {
                 val leftTests = leftMap.get(a.getTitle() + "-" + a.getFormulaTitle());
                 final List<TestcaseWeightForTestcase> collect = EntryStream
                     .of(a.getTestcaseWeights())
-                    .filter(b -> !b.getValue().equals(leftTests.get(b.getKey())))
+                    .filter(b -> Math.abs(b.getValue().getTestcaseWeight() - leftTests.get(b.getKey()).getTestcaseWeight()) > epsilon)
                     .map(Map.Entry::getValue)
                     .toImmutableList();
                 return new TestcaseWeightForProgram(a.getTitle(), a.getFormulaTitle(), collect);
---
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/temporary/OldAveragePerformanceResolver.java
@@ -0,0 +1,138 @@
+package edu.nwpu.machunyan.theoreticalEvaluation.application.temporary;
+
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorFormula;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorResolver;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.SuspiciousnessFactorForStatement;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.VectorTableModelForProgram;
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.VectorTableModelForStatement;
+
+import java.util.*;
+
+/**
+ * 重构之前的代码
+ */
+public class OldAveragePerformanceResolver {
+
+    /**
+     * 获取 vtm 的 average performance。如果vtm中没有满足要求的语句（即所有执行过的语句都是 anf != 0的），返回 0
+     *
+     * @param vtm
+     * @param sfFormula 用来计算 SuspiciousnessFactor 的公式
+     * @return
+     */
+    public static double resolve(VectorTableModelForProgram vtm, SuspiciousnessFactorFormula sfFormula) {
+        return resolve(vtm.getRecords(), sfFormula);
+    }
+
+    /**
+     * 获取 vtm 的 average performance。如果vtm中没有满足要求的语句（即所有执行过的语句都是 anf != 0的），返回 0
+     *
+     * @param vtmRecords
+     * @param sfFormula  用来计算 SuspiciousnessFactor 的公式
+     * @return
+     */
+    public static double resolve(
+        List<VectorTableModelForStatement> vtmRecords,
+        SuspiciousnessFactorFormula sfFormula) {
+
+        return resolve(
+            vtmRecords,
+            SuspiciousnessFactorResolver
+                .builder()
+                .formula(sfFormula)
+                .sort(true)
+                .build());
+    }
+
+    public static double resolve(
+        List<VectorTableModelForStatement> vtmRecords,
+        SuspiciousnessFactorResolver sfResolver) {
+
+        final List<SuspiciousnessFactorForStatement> sfs = sfResolver.resolve(vtmRecords);
+        final Map<Integer, Double> examScore = resolveExamScore(sfs, sfResolver.isSort());
+
+        return vtmRecords.stream()
+            .filter(Objects::nonNull)
+            .filter(OldAveragePerformanceResolver::isStatementNeeded)
+            .map(VectorTableModelForStatement::getStatementIndex)
+            .map(examScore::get)
+            .mapToDouble(Double::doubleValue)
+            .average()
+            .orElse(0);
+    }
+
+    private static boolean isStatementNeeded(VectorTableModelForStatement record) {
+        return record.getUnWeightedAep() + record.getUnWeightedAef() > 0
+            && record.getUnWeightedAnf() == 0;
+    }
+
+    /**
+     * 使用 EXAM 公式计算语句的代价
+     *
+     * @param sfs
+     * @return key 是语句编号， value 是结果
+     */
+    private static Map<Integer, Double> resolveExamScore(List<SuspiciousnessFactorForStatement> sfs, boolean inputSorted) {
+
+        final int n = sfs.size();
+
+        final List<SuspiciousnessFactorForStatement> sortedInput;
+        if (inputSorted) {
+            sortedInput = sfs;
+        } else {
+            sortedInput = new ArrayList<>(sfs);
+            sortedInput.sort(Comparator.comparingDouble(
+                SuspiciousnessFactorForStatement::getSuspiciousnessFactor)
+                .reversed());
+        }
+
+        final HashMap<Integer, Double> result = new HashMap<>();
+
+        // 当前元素的开始位置（sf一样的元素）
+        int rankBeginIdx = 0;
+        // 当前元素的截止位置的下一个元素的位置
+        int rankEndNextIdx = 0;
+
+        while (rankEndNextIdx < sortedInput.size()) {
+
+            if (sortedInput.get(rankBeginIdx).getSuspiciousnessFactor() ==
+                sortedInput.get(rankEndNextIdx).getSuspiciousnessFactor()) {
+
+                ++rankEndNextIdx;
+            } else {
+                resolveAndFillResult(sortedInput, rankBeginIdx, rankEndNextIdx, result, n);
+                rankBeginIdx = rankEndNextIdx;
+                ++rankEndNextIdx;
+            }
+        }
+
+        resolveAndFillResult(sortedInput, rankBeginIdx, rankEndNextIdx, result, n);
+        return result;
+    }
+
+    /**
+     * 用来减少代码重复的函数，和 {@link OldAveragePerformanceResolver#resolveExamScore(List, boolean)} )} 一起看
+     *
+     * @param rankBeginIdx
+     * @param rankEndNextIdx
+     * @param result
+     */
+    private static void resolveAndFillResult(
+        List<SuspiciousnessFactorForStatement> sortedInput,
+        int rankBeginIdx,
+        int rankEndNextIdx,
+        Map<Integer, Double> result,
+        int n) {
+
+        //noinspection UnnecessaryLocalVariable
+        final double g = rankBeginIdx;
+        final int thisRankCount = rankEndNextIdx - rankBeginIdx;
+        // 如果没有和它相同的， e=0，否则 e=thisRankCount
+        final double e = thisRankCount > 1 ? thisRankCount : 0;
+        final double ap = (g + e / 2) / n;
+
+        for (int i = rankBeginIdx; i < rankEndNextIdx; i++) {
+            result.put(sortedInput.get(i).getStatementIndex(), ap);
+        }
+    }
+}
