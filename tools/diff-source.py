af57d73e242ed64ff5251a8420a3300612903071
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-04-15 17:27:59 +0800

    让 diff-source 可以用在所有程序上
    顺便更新一下 tot_info 的 diff 结果

--- tools/diff-source.py
+++ tools/diff-source.py
@@ -3,23 +3,28 @@
 # 假如您使用的是 msys2，可以使用以下命令安装 diffutils： pacman -S diffutils
 
 from os import path
+import os
 import subprocess
 import xml.etree.ElementTree as ET
 
-base_dir = 'd:/Sources/project/theoretical-evaluation/target/sources/totinfo/'
-orig_file = path.join(base_dir, 'source.alt/source.orig/tot_info.c')
-versions_dir = path.join(base_dir, 'versions.alt/versions.orig')
+program_name = 'tot_info'
+
+base_dir = 'd:/Sources/project/theoretical-evaluation/src/main/resources/'+program_name
+orig_file = path.join(base_dir, 'origin/' +program_name+ '.c')
+versions_dir = path.join(base_dir, 'versions')
 
 root = ET.Element('root')
 
-for i in range(1, 23 + 1):
-    version_dir = 'v' + str(i)
-    vers_file = path.join(versions_dir, version_dir, 'tot_info.c')
-    result = subprocess.run(['diff', orig_file, vers_file], stdout=subprocess.PIPE)
-    
+# 按照版本名排序
+dirs = sorted(os.listdir(versions_dir), key=lambda s: s[1:].zfill(2))
+
+for version_dir in dirs:
+    vers_file = path.join(versions_dir, version_dir, program_name+'.c')
+    result = subprocess.run(['diff', '--strip-trailing-cr', orig_file, vers_file], stdout=subprocess.PIPE)
+
     ver_element = ET.SubElement(root, 'version', {"name": version_dir})
     ver_element.text = result.stdout.decode('utf-8')
-    
+
 def indent(elem, level=0):
     """
     用于缩进 xml，来自 https://stackoverflow.com/a/33956544
