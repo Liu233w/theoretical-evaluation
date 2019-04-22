d1463b355f130ed805e13996639689dc27ef08e6
Liu233w <wwwlsmcom@outlook.com>
2019-04-22 19:03:58 +0800

    简化输出的测试用例权重排名
    让同一个版本的多个公式合成一个文件。否则生成出的文件太大了。

--- src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/ResolveSortedTestcaseWeight.java
+++ src/main/java/edu/nwpu/machunyan/theoreticalEvaluation/application/ResolveSortedTestcaseWeight.java
@@ -1,17 +1,15 @@
 package edu.nwpu.machunyan.theoreticalEvaluation.application;
 
+import edu.nwpu.machunyan.theoreticalEvaluation.analyze.SuspiciousnessFactorFormulas;
 import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.TestcaseWeightForProgram;
-import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.TestcaseWeightForTestcase;
 import edu.nwpu.machunyan.theoreticalEvaluation.analyze.pojo.TestcaseWeightJam;
 import edu.nwpu.machunyan.theoreticalEvaluation.runner.TestcaseResolver;
 import edu.nwpu.machunyan.theoreticalEvaluation.runner.pojo.TestcaseItem;
-import edu.nwpu.machunyan.theoreticalEvaluation.utils.CsvExporter;
-import edu.nwpu.machunyan.theoreticalEvaluation.utils.CsvLine;
-import edu.nwpu.machunyan.theoreticalEvaluation.utils.FileUtils;
-import edu.nwpu.machunyan.theoreticalEvaluation.utils.LogUtils;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import edu.nwpu.machunyan.theoreticalEvaluation.utils.*;
+import lombok.Value;
+import one.util.streamex.EntryStream;
 import one.util.streamex.IntStreamEx;
+import one.util.streamex.StreamEx;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -20,6 +18,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 获取根据权重排名过的测试用例，便于观察结果
@@ -39,6 +38,8 @@ public class ResolveSortedTestcaseWeight {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
 
+        final String[] formulas = SuspiciousnessFactorFormulas.getAllFormulas().keySet().toArray(new String[0]);
+
         for (String name : MAIN_LIST) {
 
             final Path programDir = OUTPUT_DIR.resolve(name);
@@ -49,35 +50,62 @@ public class ResolveSortedTestcaseWeight {
 
             LogUtils.logInfo("Working on " + name);
 
-            final TestcaseWeightJam weights = ResolveTestcaseWeight.getResultFromFile(name);
+            final TestcaseWeightJam jam = ResolveTestcaseWeight.getResultFromFile(name);
             final List<TestcaseItem> testcases = TestcaseResolver.resolve(name);
 
-            for (TestcaseWeightForProgram weight : weights.getTestcaseWeightForPrograms()) {
-                final OutputItem[] outputItems = resolveForProgram(weight.getTestcaseWeights(), testcases);
+            for (Map.Entry<String, TestcaseWeightForProgram[]> entry : getProgramToFormulas(jam.getTestcaseWeightForPrograms()).entrySet()) {
+                String programTitle = entry.getKey();
+                TestcaseWeightForProgram[] weights = entry.getValue();
 
-                final Path resFile = programDir.resolve(
-                    weight.getFormulaTitle() + "-" + weight.getTitle() + ".csv");
-                FileUtils.saveString(resFile, toCsvString(outputItems));
+                final OutputItem[] outputItems = resolveForProgram(weights, testcases);
+
+                final Path resFile = programDir.resolve(programTitle + ".csv");
+                FileUtils.saveString(resFile, toCsvString(outputItems, formulas));
             }
         }
     }
 
-    private static String toCsvString(OutputItem[] list) {
+    /**
+     * 获取标题对应各个公式的map
+     *
+     * @param list
+     * @return
+     */
+    private static Map<String, TestcaseWeightForProgram[]> getProgramToFormulas(
+        List<TestcaseWeightForProgram> list) {
+
+        return StreamEx
+            .of(list)
+            .map(TestcaseWeightForProgram::getTitle)
+            .toSetAndThen(StreamEx::of)
+            .mapToEntry(title -> StreamEx.of(list)
+                .filterBy(TestcaseWeightForProgram::getTitle, title))
+            .mapValues(a -> a.toArray(TestcaseWeightForProgram[]::new))
+            .toMap();
+    }
+
+    private static String toCsvString(OutputItem[] list, String[] formulas) {
 
         final ArrayList<CsvLine> csvLines = new ArrayList<>();
 
-        csvLines.add(new CsvLine(new Object[]{
-            "rank", "weight", "name", "params", "input", "should output"
-        }));
+        csvLines.add(new CsvLine(ArrayUtils.concat(formulas,
+            new Object[]{"name", "params", "input", "should output"}
+        )));
 
         for (OutputItem item : list) {
             final TestcaseItem testcaseItem = item.getTestcaseItem();
-            csvLines.add(new CsvLine(new Object[]{
-                item.getRank(), item.getWeight(),
+            final Map<String, Double> formulaToWeight = item.getFormulaToWeight();
+
+            final Object[] weights = new Object[formulas.length];
+            for (int i = 0; i < formulas.length; i++) {
+                weights[i] = formulaToWeight.get(formulas[i]);
+            }
+
+            csvLines.add(new CsvLine(ArrayUtils.concat(weights, new Object[]{
                 testcaseItem.getName(),
                 testcaseItem.getInput(), toString(testcaseItem.getParams()),
                 testcaseItem.getOutput(),
-            }));
+            })));
         }
 
         return CsvExporter.toCsvString(csvLines);
@@ -91,41 +119,39 @@ public class ResolveSortedTestcaseWeight {
         return sb.toString();
     }
 
+    /**
+     * @param weights   一个版本的所有公式得到的权重结果
+     * @param testcases
+     * @return
+     */
     private static OutputItem[] resolveForProgram(
-        List<TestcaseWeightForTestcase> weights,
+        TestcaseWeightForProgram[] weights,
         List<TestcaseItem> testcases) {
 
-        if (weights.size() != testcases.size()) {
+        if (weights[0].getTestcaseWeights().size() != testcases.size()) {
             throw new IllegalArgumentException("数量不一致");
         }
 
-        final OutputItem[] outputItems = IntStreamEx.range(0, weights.size())
-            .mapToObj(i -> new OutputItem(
-                testcases.get(i),
-                weights.get(i).getTestcaseWeight(),
-                0))
-            // weight都大于0，相反数一下就变成从大到小排序了
-            .sortedBy(a -> -a.getWeight())
+        final Map<String, TestcaseWeightForProgram> formulaToResult = StreamEx.of(weights)
+            .toMap(TestcaseWeightForProgram::getFormulaTitle, a -> a);
+
+        return IntStreamEx
+            .range(0, testcases.size())
+            .mapToObj(i -> {
+                final TestcaseItem testcaseItem = testcases.get(i);
+                final Map<String, Double> formulaToWeight = EntryStream
+                    .of(formulaToResult)
+                    .mapValues(a ->
+                        a.getTestcaseWeights().get(i).getTestcaseWeight())
+                    .toMap();
+                return new OutputItem(testcaseItem, formulaToWeight);
+            })
             .toArray(OutputItem[]::new);
-
-        int lastRank = 0;
-        double lastWeight = Double.MAX_VALUE;
-        for (OutputItem outputItem : outputItems) {
-            if (outputItem.getWeight() < lastWeight) {
-                lastWeight = outputItem.getWeight();
-                ++lastRank;
-            }
-            outputItem.setRank(lastRank);
-        }
-
-        return outputItems;
     }
 
-    @Data
-    @AllArgsConstructor
+    @Value
     private static class OutputItem {
         TestcaseItem testcaseItem;
-        double weight;
-        int rank;
+        Map<String, Double> formulaToWeight;
     }
 }
