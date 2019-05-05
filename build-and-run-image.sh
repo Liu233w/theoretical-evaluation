11f949c062e1181087c9d647162010cdcaf86600
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-05-05 20:34:54 +0800

    精简运行程序
    不需要单独构建 docker 镜像了，还是直接构建并运行

---
+++ build-and-run-image.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# 构建并运行镜像，将使用当前目录作为结果目录
+set -e
+
+export DOCKER_BUILDKIT=0
+
+IMAGE=theoretical-evaluation
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t ${IMAGE} ${DIR}
+
+DOCKER_ENV_PARAM=""
+# un-comment the line below to disable parallel
+#DOCKER_ENV_PARAM="${DOCKER_ENV_PARAM} -e DISABLE_PARALLEL=1"
+
+docker run -m 5.5GB -it --rm -v ${DIR}:/app/target/outputs ${DOCKER_ENV_PARAM} ${IMAGE} $1
